bba323d1319db542e54638f1dcb2a320106cb80d
hifly81 <gio.marigi@gmail.com>
2011-03-17 13:53:52 +0100

    baseline src files

---
+++ src/main/java/it/gmarigi/jmedia/xrunner/Runner.java
@@ -0,0 +1,25 @@
+/*
+ * Created on 29-gen-2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package it.gmarigi.jmedia.xrunner;
+
+import it.gmarigi.jmedia.gui.*;
+
+/**
+ * @author HiFly
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Runner {
+
+	public static void main(String[] args) throws Exception {
+		XPlayer xp = new XPlayer();
+		xp.show();
+		
+	
+	}
+}
